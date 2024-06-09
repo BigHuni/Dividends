2c767550e829b2a75b24a450adc6e802a244750d
BigHuni <hdh8659@gmail.com>
2024-06-10 08:59:31 +0900

    [ADD] 배당금 저장 API 코드 추가

---
+++ src/main/java/com/dayone/model/Company.java
@@ -0,0 +1,24 @@
+package com.dayone.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private String ticker;
+    private String name;
+}
